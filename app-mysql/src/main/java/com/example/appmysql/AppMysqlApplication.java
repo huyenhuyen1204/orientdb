0ff362c86d3c0aa230f1fe0e8fcb013f0d6b6fbd
huyenhz <nguyenhuyen98pm@gmail.com>
2018-11-12 23:58:36 +0700

    first

---
+++ app-mysql/src/main/java/com/example/appmysql/AppMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.appmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMysqlApplication.class, args);
+    }
+}
---
+++ app-orientdb/src/main/java/com/example/apporientdb/AppOrientdbApplication.java
@@ -0,0 +1,12 @@
+package com.example.apporientdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOrientdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppOrientdbApplication.class, args);
+    }
+}
