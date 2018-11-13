5e62093af5acc1f8637c430a187670bdae694a66
huyenhz <nguyenhuyen98pm@gmail.com>
2018-11-13 10:15:08 +0700

    config multi modules

---
+++ app-mysql/src/main/java/com/example/appmysql/entry/NameBasics.java
@@ -0,0 +1,13 @@
+package com.example.appmysql.entry;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "name.basics")
+public class NameBasics {
+
+
+    private String nconts;
+}
