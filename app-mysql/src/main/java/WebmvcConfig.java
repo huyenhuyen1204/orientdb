6952dfaccf53d1c109cbac1f15e23af3fbb90770
huyenhz <nguyenhuyen98pm@gmail.com>
2018-12-02 00:21:55 +0700

    401 Autho

---
+++ app-mysql/src/main/java/WebmvcConfig.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@EnableWebMvc
+public class WebmvcConfig  extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**");
+    }
+}
