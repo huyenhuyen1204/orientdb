0d16dac464913dc46a237da4c56ed9528d024130
huyenhz <nguyenhuyen98pm@gmail.com>
2018-12-02 09:04:19 +0700

    final

--- app-mysql/src/main/java/com/example/appmysql/AppMysqlApplication.java
+++ app-mysql/src/main/java/com/example/appmysql/AppMysqlApplication.java
@@ -3,9 +3,15 @@ package com.example.appmysql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class AppMysqlApplication {
@@ -19,13 +25,39 @@ public class AppMysqlApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedHeaders("header1", "header2", "header3")
+//                        .exposedHeaders("header1", "header2")
+//                        .allowCredentials(false).maxAge(3600);
                 registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("header1", "header2", "header3")
-                        .exposedHeaders("header1", "header2")
-                        .allowCredentials(false).maxAge(3600);
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
             }
         };
     }
+//@Bean
+//public WebMvcConfigurer corsConfigurer() {
+//    return new WebMvcConfigurerAdapter() {
+//        @Override
+//        public void addCorsMappings(CorsRegistry registry) {
+//            registry.addMapping("/**").allowedOrigins("*");
+//        }
+//    };
+//}
+@Bean
+public CorsConfigurationSource corsConfigurationSource() {
+    final CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("HEAD",
+            "GET", "POST", "PUT", "DELETE", "PATCH"));
+    // setAllowCredentials(true) is important, otherwise:
+    // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+    configuration.setAllowCredentials(true);
+    // setAllowedHeaders is important! Without it, OPTIONS preflight request
+    // will fail with 403 Invalid CORS request
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);    return source;
+}
 }
