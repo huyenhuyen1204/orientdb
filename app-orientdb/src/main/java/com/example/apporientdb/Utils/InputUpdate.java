15f6411a6303ff78cd5460a9f0a36eaed2ebb8d3
huyenhuyen1204 <nguyenhuyen98pm@egmail.com>
2018-12-05 08:28:35 +0700

    edit somethings

---
+++ app-orientdb/src/main/java/com/example/apporientdb/Utils/InputUpdate.java
@@ -0,0 +1,22 @@
+package com.example.apporientdb.Utils;
+
+public class InputUpdate {
+    private String nconst;
+    private String category;
+
+    public String getNconst() {
+        return nconst;
+    }
+
+    public void setNconst(String nconst) {
+        this.nconst = nconst;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
